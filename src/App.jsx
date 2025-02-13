34e26e7f4e04ab1caf33975082c25a02cba10622
allieoc <allieocon@gmail.com>
2025-02-13 11:22:50 -0500

    set up project with react and created header and footer components

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+
+  return (
+   <BrowserRouter>
+   <Header />
+    <Routes>
+      
+    </Routes>
+   <Footer />
+   </BrowserRouter>
+  )
+}
+
+export default App
