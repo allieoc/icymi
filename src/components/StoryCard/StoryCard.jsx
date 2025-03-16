f3baf7b1f6d4e796f6d928ca864623b314bf74d4
allieoc <allieocon@gmail.com>
2025-03-16 17:35:21 -0400

    set up storycard

---
+++ src/components/StoryCard/StoryCard.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom'
+import React, {useState} from 'react'
+
+function StoryCard() {
+  return (
+    <div>StoryCard</div>
+  )
+}
+
+export default StoryCard
--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -2,29 +2,11 @@ import React, {useContext, useEffect, useState} from "react";
 
 function Homepage({ apiKey, baseUrl }) {
 
-    const [topStories, setTopStories] = useState([]);
-
-    useEffect(()=>{
-        const fetchTopStories = async () => {
-            try{
-                const topStoriesResponse = await axios.get(
-                    `${baseUrl}/topstories/v2/home.json?api-key=${apiKey}`
-                );
-                console.log(topStoriesResponse)
-               // setTopStories(topStoriesResponse.data.results)
-            }
-            catch(err){
-                console.log(err)
-            }
-        }
-        fetchTopStories();
-    }, [])
 
     return (
         <div>
-            {
-                topStories.map()
-            }
+            <StoryCard />
+            
         </div>
 
     );
